778e7c7e6fd55495e61d3add2e7e656a2fe82ec8
PRAGHATIESH S <73994558+praghatiesh@users.noreply.github.com>
2021-06-25 18:21:24 +0530

    70vh

--- src/component/Home.js
+++ src/component/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
           style={{
             display: "flex",
             flexDirection: "column",
-            height:70vh,
+//             height:70vh,
             justifyContent: "center",
             alignItems: "center",
           }}
